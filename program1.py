a0ebd306e5d49d5ee5f57a3d08ba82a2758e79ae
naveen <naveennn398222@gmail.com>
2025-11-20 14:51:02 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,13 +1,10 @@
 import sys
 if len(sys.argv) !=3:
-  print("usage:python program1.py <name> <roll>
-        sys.exit(1)
-script_name=sys.argv[0]
-name=sys.argv[1]
-rollno=sys.argv[2]
-print("script Name:",script_name)
-print("Student Name:",name)
-print("roll number:",rollno)
-
-
-
+  print("Usage : python details.py <name> <rollno>")
+  sys.exit(1)
+script_name =sys.argv[0]
+name =sys.argv[1]
+rollno =sys.argv[2]
+print("student name :",script_name)
+print("student name :",name)
+print("Roll No. :",rollno)
